c4b4f21baa4f2c14aa833eab3e089cc017812feb
Raj Pancholi <pancholiraj27@gmail.com>
2022-06-22 16:38:40 +0530

    display given todo and also add dummy trash and done button

--- app.js
+++ app.js
@@ -13,4 +13,27 @@ todoButton.addEventListener("click", submitForm);
 
 function submitForm(e) {
   e.preventDefault();
+  // creating new li
+  const newLi = document.createElement("li");
+  const newTodo = "hello";
+  newLi.append(newTodo);
+  // console.log(newLi);
+
+  // adding done button
+  const todoDone = document.createElement("button");
+  todoDone.innerText = "✅";
+  // creating class name of button
+  todoDone.setAttribute("class", "todoWorkDone");
+  newLi.append(todoDone);
+
+  // adding trash button
+  const trash = document.createElement("button");
+  trash.innerText = "❌";
+  // creating class name of button
+  trash.setAttribute("class", "trashWork");
+  newLi.append(trash);
+
+  // adding new li to ul
+  todoList.append(newLi);
+  // console.log(newLi);
 }
